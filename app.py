2b878d214fddb00da2e7a245af2f5713bf325a21
Arthur Stephan <arthurstephan13@gmail.com>
2020-07-07 20:41:40 -0300

    fixbugs

--- app.py
+++ app.py
@@ -108,5 +108,5 @@ def deletar_tarefa(nome_tarefa,id_usuario):
 
 
 if __name__ =="__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port,debug=True)
+    app.run(debug=True)
+    
